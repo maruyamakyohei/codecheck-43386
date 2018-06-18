58d0a034d111d4142927c69885798661117c89d0
tie303310 <tie303310@S1504C10034-T1.tisnt.tis.co.jp>
2018-06-19 01:45:10 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -7,44 +7,52 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class App {
-	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
+    public static void main(String[] args) {
 
-			try {
-				URL urlObj = new URL("http://challenge-server.code-check.io/api/hash?q=" + args[i]);
-		        HttpURLConnection http = (HttpURLConnection) urlObj.openConnection();
-		        http.setRequestMethod("GET");
-		        http.connect();
+    	String param = null;
 
-		         // HTTPレスポンスコード
-	            final int status = http.getResponseCode();
-	            if (status == HttpURLConnection.HTTP_OK) {
-	                // 通信に成功した
-	                // テキストを取得する
-	                final InputStream in = http.getInputStream();
+        for (int i = 0, l = args.length; i < l; i++) {
 
-	                String encoding = http.getContentEncoding();
-	                if(null == encoding){
-	                    encoding = "UTF-8";
-	                }
+        	param = param + args[i];
+        }
 
-	                final InputStreamReader inReader = new InputStreamReader(in, encoding);
-	                final BufferedReader bufReader = new BufferedReader(inReader);
-	                String line = null;
-	                // 1行ずつテキストを読み込む
-	                while((line = bufReader.readLine()) != null) {
-	                	String[] hash = line.split("\"");
-	                	System.out.println(hash[7]);
-	                }
-	                bufReader.close();
-	                inReader.close();
-	                in.close();
-	            }else{
-	                System.out.println(status);
-	            }
-			} catch (Exception e1) {
-                e1.printStackTrace();
+        param = param.replace("+", "%27");
+        param = param.replace("\'", "%2B");
+
+        try {
+            URL urlObj = new URL("http://challenge-server.code-check.io/api/hash?q=" + param);
+            HttpURLConnection http = (HttpURLConnection) urlObj.openConnection();
+            http.setRequestMethod("GET");
+            http.connect();
+
+            // HTTPレスポンスコード
+            final int status = http.getResponseCode();
+            if (status == HttpURLConnection.HTTP_OK) {
+                // 通信に成功した
+                // テキストを取得する
+                final InputStream in = http.getInputStream();
+
+                String encoding = http.getContentEncoding();
+                if(null == encoding){
+                    encoding = "UTF-8";
+                }
+
+                final InputStreamReader inReader = new InputStreamReader(in, encoding);
+                final BufferedReader bufReader = new BufferedReader(inReader);
+                String line = null;
+                // 1行ずつテキストを読み込む
+                while((line = bufReader.readLine()) != null) {
+                    String[] hash = line.split("\"");
+                    System.out.println(hash[7]);
+                }
+                bufReader.close();
+                inReader.close();
+                in.close();
+            }else{
+                System.out.println(status);
             }
-		}
-	}
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
 }
